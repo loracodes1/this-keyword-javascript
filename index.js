f465185487e0d640daedf3dea2513aa65db5a5a2
florence <loracodes3@gmail.com>
2024-10-14 12:44:48 +0300

    vbjhg

---
+++ index.js
@@ -0,0 +1,8 @@
+// this is a keyword that refers to the object that is executing the current function. Its value can change based on how the function is called.
+
+//1.Global Context
+
+// When you use this outside any function, it refers to the global object (in browsers, that's the windoclear
+
+
+console.log(this); // In the browser, this logs the Window object
